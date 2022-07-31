89bb2aa325ee4a436c4c0d9ec2b69502471ad56d
Shivam-kishor <100480064+Shivam-kishor@users.noreply.github.com>
2022-07-31 14:06:23 +0530

    Create datatype.py

---
+++ datatype.py
@@ -0,0 +1,34 @@
+
+
+
+x= 1920 
+y = 2094
+x2 =-54.e10000
+y3 ='si'
+z1=None
+print(x, type(x))
+print(y, type(y))
+print(x2, type(x2))
+print(y3, type(y3))
+print(z1, type(z1))
+#list is an orderd sequence of itmes/
+#most used
+#Items in a list do not to be a same type 
+#declaring is straigh ForkProcess 
+#seprated by commas  and enclosed  with []
+#example a=[1,"fe".]
+list=[1,2,"set h"]
+tupple=(2.3,4,'tupple')
+set={1,3,3,3,5,"set",'set h'}
+print(list)
+print(tupple)
+print(set)
+#dict={ 1: ="1 hai",2='2 hai'}
+x1=9948349
+y23=4 
+y11=83
+print(x1//y23)#interger divison
+print(x1/y11)#float divison
+guys=['apple',"banana","kf",'fe']
+print('kf'in guys)
+print('samosa'in guys)
